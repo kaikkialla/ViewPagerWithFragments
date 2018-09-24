775774c44545d55f1ed56c49683171c57d1e645d
kuelye <kuelye@gmail.com>
2018-09-24 20:58:45 +0300

    add ViewPager and CircleIndicator

--- app/src/main/java/com/banana/y17_2/promo/MainActivity.java
+++ app/src/main/java/com/banana/y17_2/promo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.banana.y17_2.promo;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        FragmentsAdapter adapter = new FragmentsAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
     }
+
 }
