e4ee17b3fade9bf196dc0e84d1909a42e9029526
tiget <tigetxd@mail.com>
2018-09-27 21:00:20 +0300

    Merge remote-tracking branch 'origin/master' into dev_tiget
    # Conflicts:
    #	app/src/main/java/com/banana/y17_2/promo/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/banana/y17_2/promo/MainActivity.java
+++ app/src/main/java/com/banana/y17_2/promo/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,8 +19,10 @@ import me.relex.circleindicator.CircleIndicator;
 public class MainActivity extends AppCompatActivity {
     TextView My_Account_Button;
     ImageView Filters_Button;
-    Button Discover_More_Buton;
+    Button Discover_More_Button;
+    Timer swipeTimer;
 
+    ViewPager viewPager;
 
 
     @Override
@@ -25,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ViewPager viewPager = findViewById(R.id.View_Pager);
+        viewPager = findViewById(R.id.View_Pager);
         final ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
 
@@ -34,30 +39,99 @@ public class MainActivity extends AppCompatActivity {
 
         My_Account_Button = findViewById(R.id.My_Account_Button);
         Filters_Button = findViewById(R.id.Filters_Button);
-        Discover_More_Buton = findViewById(R.id.Discover_More_Button);
+        Discover_More_Button = findViewById(R.id.Discover_More_Button);
 
-        final Handler handler = new Handler();
-        final Runnable Update = new Runnable() {
-            public void run() {
-                if (viewPager.getCurrentItem() == 6) {
-                    viewPager.setCurrentItem(0);
-                }
-                viewPager.setCurrentItem(viewPager.getCurrentItem()+1, true);
-            }
-        };
-        Timer swipeTimer = new Timer();
-        swipeTimer.schedule(new TimerTask() {
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        swipeImage();
+
+        findViewById(R.id.Root_container).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                handler.post(Update);
+            public void onClick(View view) {
+                swipeTimer.cancel();
             }
-        }, 3000, 3000);
-
+        });
 
        /*
         if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.Frame_Layout, new Fragment2(), null).commit();
         }
         */
+
+
     }
+
+    public void swipeImage() {
+        if (swipeTimer != null) {
+            swipeTimer.cancel();
+        }
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                int i = viewPager.getCurrentItem() + 1;
+                if (i > 4) {
+                    i = 0;
+                }
+                viewPager.setCurrentItem(i, true);
+                Log.e("ViewNumber", String.valueOf(viewPager.getCurrentItem()));
+            }
+        };
+        swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 5000, 5000);
+    }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) { // [-1,1]
+
+                view.setRotation(360 * position);
+
+
+
+
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
 }
+
+
+
+//[-1;1]
+
+//Modify the default slide transition to shrink the page as well
+                /* float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+                */
--- app/src/main/java/com/banana/y17_2/promo/ViewPagerAdapter.java
+++ app/src/main/java/com/banana/y17_2/promo/ViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package com.banana.y17_2.promo;
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
@@ -19,22 +20,20 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
 
     @Override
-    public Fragment getItem(final int i) {
+    public Fragment getItem(int i) {
 
 
         switch (i){
             case 0:
                 return new Fragment2();
             case 1:
-                return new Fragment2();
+                return new Dom98111Fragment();
             case 2:
-                return new Fragment2();
+                return new KuelyeFragment();
             case 3:
-                return new Fragment2();
+                return new NikitaFragment();
             case 4:
-                return new Fragment2();
-            case 5:
-                return new Fragment2();
+                return new SobolFragment();
             default:
                 //Toast.makeText(context, "Oops, Something went wrong", Toast.LENGTH_SHORT).show();
                 return null;
@@ -43,6 +42,15 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public int getCount() {
-        return 6;
+        return 5;
     }
+
+
+    //For PagerTabStrip
+/*    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.valueOf(position);
+    }
+    */
 }
